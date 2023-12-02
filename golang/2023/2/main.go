eb180a5be3e0543ba84a527f7c932a502d004f26
czyber <czypka.github@gmail.com>
2023-12-02 09:10:30 +0100

    Update aoc-downloader

---
+++ golang/2023/2/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strconv"
+	"strings"
+
+	aocdownloader "github.com/czyber/aoc-downloader"
+)
+
+func readInput() []string {
+	input, err := aocdownloader.GetInput("2023", "2")
+	if err != nil {
+		log.Fatal("Error getting input: ", err)
+	}
+
+	inputStrings := strings.Split(input, "\n")
+
+	return inputStrings
+}
+
+func extractGameID(line string) string {
+	re := regexp.MustCompile(`\d+`)
+	game := strings.Split(line, ": ")
+	if len(game) == 2 {
+		gameID := re.FindString(game[0])
+		return gameID
+	}
+	return ""
+}
+
+func extractGameData(line string) string {
+	game := strings.Split(line, ": ")
+	if len(game) == 2 {
+		return game[1]
+	}
+	return ""
+}
+
+func extractSamples(line string) []string {
+	return strings.Split(line, "; ")
+}
+
+func compareToMaxSize(size int, color string) bool {
+	maxSizeForColor := map[string]int{
+		"green": 13,
+		"red":   12,
+		"blue":  14,
+	}
+
+	return size <= maxSizeForColor[color]
+}
+
+func validateSamples(samples []string) bool {
+	re := regexp.MustCompile(`\b\d+\s(green|blue|red)\b`)
+	reDigits := regexp.MustCompile(`\d+`)
+	reColors := regexp.MustCompile(`\b(green|blue|red)\b`)
+	gameIsValid := true
+	for _, sample := range samples {
+		compiledSamples := re.FindAllString(sample, -1)
+		for _, compiledSample := range compiledSamples {
+			sampleSize := reDigits.FindString(compiledSample)
+			sampleColor := reColors.FindString(compiledSample)
+			iSampleSize, err := strconv.Atoi(sampleSize)
+
+			if err != nil {
+				log.Fatal("String to integer conversion failed: ", err)
+			}
+
+			sampleIsValid := compareToMaxSize(iSampleSize, sampleColor)
+			if !sampleIsValid {
+				gameIsValid = false
+				break
+			}
+
+		}
+	}
+	return gameIsValid
+}
+
+func main() {
+	input := readInput()
+	sum := 0
+	for _, line := range input {
+		gameID := extractGameID(line)
+		gameData := extractGameData(line)
+		samples := extractSamples(gameData)
+		gameIsValid := validateSamples(samples)
+		if gameIsValid {
+			iGameID, err := strconv.Atoi(gameID)
+			if err != nil {
+				log.Fatal("String to integer conversion failed: ", err)
+			}
+			sum += iGameID
+		}
+	}
+	fmt.Println("Result:", sum)
+}
